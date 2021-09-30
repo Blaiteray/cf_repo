775dfbd557c71ffd51bb8bf1a6c1de68bb02aac6
Blaiteray <66131047+Blaiteray@users.noreply.github.com>
2021-09-30 08:51:48 +0600

    update z

--- d.cpp
+++ d.cpp
@@ -13,27 +13,27 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 typedef vector<int> vi;//push_back(),pop_back(),back(),size(),begin(),end(),insert(ite pos, val)
-typedef vector<ll> vll;//clear(),erase(ite position),empty()
+typedef vector<vector<int>> vvi;//clear(),erase(ite position),empty()
 typedef map<int,int> mii;//begin(),end(),size(),insert(pair),insert({key,element}),find(key value)
 //erase(ite position),erase(key value),clear(),empty()
 typedef pair<int,int> pii;
 typedef priority_queue<int> pqi;//pop(),push(),size(),top()[for stl::queue it's front()],empty()
-typedef priority_queue<ll> pqll;//also for std::stack, std::queue [has extra: back()]
-typedef priority_queue<pii> pqii;
-typedef priority_queue<pii, vector<pii>, greater<pii>> pqgii;
+typedef priority_queue<pii> pqii;//also for std::stack, std::queue [has extra: back()]
 typedef priority_queue<int,vector<int>,greater<int>> pqgi;
-typedef priority_queue<ll,vector<ll>,greater<ll>> pqgl;
+typedef set<int> si;
 typedef multiset<int> msi;
 //std::set//insert(),begin(),end(),size(),find(key),clear(),erase(),empty()
 typedef tuple<int,int,int> tiii; //make_tuple(int,int,int),get<position>(name_of_tuple)
 typedef deque<int> di;//vector, with  push_front(), pop_front()
 #define pb(k) push_back(k)
 #define mp(a,b) make_pair(a,b)
-
+#define B begin();
+#define E end();
 #define nl cout<<"\n"
-#define Test(x) {static int testInt=1000;if((testInt--)>0)cout<<"(LINE "<<__LINE__<<": VALUE "<<x<<")\t";}
-#define test {static int testIntx=1000;if((testIntx--)>0)cout<<"(LINE "<<__LINE__<<": CALL "<<1000-testIntx<<")\t";}
-#define testarr(arr) {int* lLe=(int*)(&arr+1);for(int* xTe=arr;xTe!=lLe;xTe++) cout<<*xTe<<" ";nl}
+
+#define DB(x) {static int testInt=1000;if((testInt--)>0)cout<<"(LINE "<<__LINE__<<": VALUE "<<x<<")\t";}
+#define LB {static int testIntx=0;if(testIntx<1000)cout<<"(LINE "<<__LINE__<<","<<testIntx+1<<")\t";else break;testIntx++;}
+#define TA(arr) {int* lLe=(int*)(&arr+1);for(int* xTe=arr;xTe!=lLe;xTe++) cout<<*xTe<<" ";nl;}
 
 #define nax 1000000007
 
@@ -42,7 +42,7 @@ typedef deque<int> di;//vector, with  push_front(), pop_front()
 int main() {
 	FASTio
 	int t; cin >> t; while(t--) {
-		test
+		LB
 	}
 	return 0;
 }
