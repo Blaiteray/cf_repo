080ed609c1513dfb53a456eafcbb4ae7c1cb3301
Blaiteray <66131047+Blaiteray@users.noreply.github.com>
2021-12-08 03:27:34 +0600

    changing template

--- d.cpp
+++ d.cpp
@@ -2,19 +2,13 @@
 using namespace std;
 #define FASTio ios::sync_with_stdio(false);cin.tie(NULL);
 #define DECI fixed<<setprecision(5)
-
 // #include <ext/pb_ds/assoc_container.hpp> 
 // #include <ext/pb_ds/tree_policy.hpp>
 // using namespace __gnu_pbds;
 // typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
 // typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_multiset;
-
-typedef long long ll;typedef unsigned long long ull;typedef long double ld;
-typedef vector<int> vi;typedef vector<vector<int>> vvi;typedef deque<int> di;
-typedef map<int,int> mii;
-typedef pair<int,int> pii;typedef tuple<int,int,int> tiii;
-typedef priority_queue<int> pqi;typedef priority_queue<int,vector<int>,greater<int>> pqgi;
-typedef set<int> si;typedef multiset<int> msi;
+typedef long long ll;typedef vector<int> vi;typedef vector<vector<int>> vvi;typedef deque<int> di;
+typedef pair<int,int> pii;typedef priority_queue<int,vector<int>,greater<int>> pqgi;
 #define pb(k) push_back(k)
 #define mp(a,b) make_pair(a,b)
 #define B begin()
@@ -22,8 +16,6 @@ typedef set<int> si;typedef multiset<int> msi;
 #define F first
 #define S second
 #define nl cout<<"\n"
-/***************Debugging tools are below*****************/
-#define LC cout<<"line("<<__LINE__<<") ";
 #define DB(x) {static int _ti_=1000;if((_ti_--)>0)cout<<#x<<": "<<x<<"\n";}
 #define LB {static int _tx_=0;if(_tx_>=1000) {cout<<"inf loop\n" ;break;}_tx_++;}
 #define TA(a) {int* n=(int*)(&a+1);cout<<#a<<": ";for(int* i=a;i!=n;i++) cout<<*i<<" ";nl;}
@@ -32,8 +24,7 @@ typedef set<int> si;typedef multiset<int> msi;
 int main() {
 	FASTio
 	int t; cin >> t; while(t--) {
-		LB
-		LC DB(t)
+		;
 	}
 	return 0;
 }
--- zen.cpp
+++ zen.cpp
@@ -11,14 +11,6 @@ int gen(const int &a, const int &b) {
 
 int main(int argc, char* argv[]) {
 	FASTio
-	int n=1000000,d=1;
-	// srand(atoi(argv[1]));
-	srand(8);
-	cout<<1<<"\n";
-	cout<<n<<" "<<d<<"\n";
-	for(int i=0;i<n;i++) {
-		cout<<gen(0,1)<<" ";
-	}
-	cout<<"\n";
+	;
 	return 0;
 }
